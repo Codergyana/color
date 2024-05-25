4c2d69efa209e5a45fca73c00c9727372bc12e80
Codergyana <165542618+Codergyana@users.noreply.github.com>
2024-05-25 20:31:15 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,67 @@
+console.log("hello")
+let rand=Math.random()
+if(rand<0.33){
+    document.getElementById("gy").style.backgroundColor = "red"
+    document.getElementById("gy").style.color = "black"
+  }
+  else if(rand>=0.33 && rand<0.66){
+    document.getElementById("gy").style.backgroundColor = "pink"
+    document.getElementById("gy").style.color = "black"                                
+      }
+  else{
+    document.getElementById("gy").style.backgroundColor = "aqua"
+    document.getElementById("gy").style.color = "pink"
+  }
+  rand=Math.random()
+if(rand<0.33){
+    document.getElementById("gya").style.backgroundColor = "red"
+    document.getElementById("gya").style.color = "pink"
+  }
+  else if(rand>=0.33 && rand<0.66){
+    document.getElementById("gya").style.backgroundColor = "pink"
+    document.getElementById("gya").style.color = "black"                                
+      }
+  else{
+    document.getElementById("gya").style.backgroundColor = "aqua"
+    document.getElementById("gya").style.color = "pink"
+  }
+  rand=Math.random()
+if(rand<0.33){
+    document.getElementById("gyan").style.backgroundColor = "red"
+    document.getElementById("gyan").style.color = "pink"
+  }
+  else if(rand>=0.33 && rand<0.66){
+    document.getElementById("gyan").style.backgroundColor = "pink"
+    document.getElementById("gyan").style.color = "black"                                
+      }
+  else{
+    document.getElementById("gyan").style.backgroundColor = "aqua"
+    document.getElementById("gyan").style.Color = "pink"
+  }
+  rand=Math.random()
+if(rand<0.33){
+    document.getElementById("gyana").style.backgroundColor = "red"
+    document.getElementById("gyana").style.color = "pink"
+  }
+  else if(rand>=0.33 && rand<0.66){
+    document.getElementById("gyana").style.backgroundColor = "pink"
+    document.getElementById("gyana").style.color = "black"                                
+      }
+  else{
+    document.getElementById("gyana").style.backgroundColor = "aqua"
+    document.getElementById("gyana").style.Color = "pink"
+  }
+  rand=Math.random()
+if(rand<0.33){
+    document.getElementById("hh").style.backgroundColor = "red"
+    document.getElementById("hh").style.color = "pink"
+  }
+  else if(rand>=0.33 && rand<0.66){
+    document.getElementById("hh").style.backgroundColor = "pink"
+    document.getElementById("hh").style.color = "black"                                
+      }
+  else{
+    document.getElementById("hh").style.backgroundColor = "aqua"
+    document.getElementById("hh").style.Color = "pink"
+  }
+  
